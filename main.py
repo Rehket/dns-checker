8d73b35c0782e5cc983b275a3ee5a900bd15ce54
Rehket <aalbright425@gmail.com>
2019-12-02 18:02:14 -0500

    Did some formatting

--- main.py
+++ main.py
@@ -16,16 +16,18 @@ name_server_list = os.getenv("NAME_SERVER_LIST", None)
 if __name__ == "__main__":
 
     #  Parse the environment variables. It they are not setup, raise exception.
-    if name_server_list is None:
+    if name_server_list is None or name_server_list.isspace():
         raise EnvironmentError(
-            "No name servers provided! Set the NAME_SERVER_LIST environment variable to a bar(|) delimited string of name servers!"
+            "No name servers provided! Set the NAME_SERVER_LIST "
+            "environment variable to a bar(|) delimited string of name servers!"
         )
     name_server_list = name_server_list.split("|")
     logging.info(f"Name Servers: {name_server_list}")
 
-    if address_list is None:
+    if address_list is None or address_list.isspace():
         raise EnvironmentError(
-            "No address list provided! Set the ADDRESS_LIST environment variable to a bar(|) delimited string of addresses!"
+            "No address list provided! Set the ADDRESS_LIST "
+            "environment variable to a bar(|) delimited string of addresses!"
         )
     address_list = address_list.split("|")
     logging.info(f"Address List: {address_list}")
