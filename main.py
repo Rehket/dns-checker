1c0c54b148c55b164c87dcd71c4a85d020883626
Rehket <aalbright425@gmail.com>
2019-12-02 17:59:13 -0500

    Added some comments and actuall started using the nameserver list

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import dns.resolver
 import logging
 import os
 import sys
+
+#  Setup some basic logging.
 logging.basicConfig(level=logging.INFO)
 
+# Get the important Environment Variables.
 address_list = os.getenv("ADDRESS_LIST", None)
 # address_list = os.getenv("ADDRESS_LIST", "google.com|officedepot.com|bad_broken.add.ress")
 
@@ -12,31 +15,33 @@ name_server_list = os.getenv("NAME_SERVER_LIST", None)
 
 if __name__ == "__main__":
 
+    #  Parse the environment variables. It they are not setup, raise exception.
     if name_server_list is None:
-        logging.error("No name servers provided!"
-                      "Set the NAME_SERVER_LIST environment variable to a bar(|) delimited string of name servers!")
-        sys.exit(-1)
+        raise EnvironmentError(
+            "No name servers provided! Set the NAME_SERVER_LIST environment variable to a bar(|) delimited string of name servers!"
+        )
     name_server_list = name_server_list.split("|")
     logging.info(f"Name Servers: {name_server_list}")
 
     if address_list is None:
-        logging.error("No address list provided!"
-                      "Set the ADDRESS_LIST environment variable to a bar(|) delimited string of addresses!")
-        sys.exit(-1)
+        raise EnvironmentError(
+            "No address list provided! Set the ADDRESS_LIST environment variable to a bar(|) delimited string of addresses!"
+        )
     address_list = address_list.split("|")
     logging.info(f"Address List: {address_list}")
 
+    # Setup the Resolver.
     my_resolver = dns.resolver.Resolver(configure=False)
-    my_resolver.nameservers = ["1.1.1.1", "8.8.8.8"]
+    my_resolver.nameservers = name_server_list
 
     for address in address_list:
         try:
             result = my_resolver.query(address)
-            logging.info(f"Response for {address} canonical_name: {result.canonical_name}")
+            logging.info(
+                f"Response for {address} canonical_name: {result.canonical_name}"
+            )
         except dns.resolver.NXDOMAIN as e:
             logging.error(f"{address} could not be resolved! Sound the alarm!")
         except Exception as e:
             logging.error(f"Something unexpected broke! Sound the alarm!")
             logging.error(e)
-
-
